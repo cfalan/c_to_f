2de6b15212cbc2f1d2535a5455128877bdb72b77
Alan Chan <cf.alanpang@gmail.com>
2023-05-16 09:18:57 +0800

    add new file

---
+++ lesson_36.py
@@ -0,0 +1,19 @@
+
+
+
+
+
+
+C = float(input('溫度是多少？ '))
+
+
+
+F = C * 9 / 5 + 32
+
+print ('華氏溫度是： ',F)
+
+
+
+#python3 lesson_36.py
+#cd /users/alanchan/documents/coding
+#cd /users/alanchan/desktop
